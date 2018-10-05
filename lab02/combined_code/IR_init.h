450f160106648d3d466ed4e5aedc2a45f4095f5d
xy97cornell <xy97@cornell.edu>
2018-10-05 12:04:51 -0400

    added integration code

---
+++ lab02/combined_code/IR_init.h
@@ -0,0 +1,12 @@
+
+
+int N = 25;
+uint32_t target_freq = 6080;
+uint32_t smp_freq = 19235*2;
+
+uint32_t sum = 0;
+
+
+void ir_init(){
+;    
+}
---
+++ lab02/combined_code/combined_code.ino
@@ -0,0 +1,69 @@
+#define LOG_OUT 1 // use the log output function
+#define FFT_N 256 // set to 256 point fft
+
+
+#include <FFT.h> // include the library
+#include "IR_init.h"
+
+
+uint8_t state;
+
+void turn_led(int i){
+  int pin = LED_BUILTIN;
+  pinMode(pin,OUTPUT);
+  i ? digitalWrite(pin,HIGH) : digitalWrite(pin, LOW); 
+}
+
+
+void setup() {
+  Serial.begin(115200); // use the serial port
+  TIMSK0 = 0; // turn off timer0 for lower jitter
+  ADCSRA = 0xe5; // set the adc to free running mode
+  ADMUX = 0x40; // use adc0
+  DIDR0 = 0x01; // turn off the digital input for adc0
+  
+}
+
+void loop() {
+  switch (state){
+        
+        case 0:
+            
+            for(int j=0;j<N;j++) { // reduces jitter
+                cli();  // UDRE interrupt slows this way down on arduino1.0
+                for (int i = 0 ; i < 512 ; i += 2) { // save 256 samples
+                    while(!(ADCSRA & 0x10)); // wait for adc to be ready
+                    ADCSRA = 0xf5; // restart adc
+                    byte m = ADCL; // fetch adc data
+                    byte j = ADCH;
+                    int k = (j << 8) | m; // form into an int
+                    k -= 0x0200; // form into a signed int
+                    k <<= 6; // form into a 16b signed int
+                    fft_input[i] = k; // put real data into even bins
+                    fft_input[i+1] = 0; // set odd bins to 0
+                }
+                fft_window(); // window the data for better frequency response
+                fft_reorder(); // reorder the data before doing the fft
+                fft_run(); // process the data in the fft
+                fft_mag_log(); // take the output of the fft
+                sei();   
+                
+                sum += fft_log_out[40];
+            }
+            state ++;
+            break;
+
+        case 1:
+            sum/=N;
+            Serial.println(sum);
+            if(sum>55){
+                turn_led(1);
+            }else{
+                turn_led(0);
+            }
+            sum = 0;
+            state = 0;
+            break;
+  }  
+ 
+}
