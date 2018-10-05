e8974f77eb5e504dd435fa2706f807368b20b708
xy97cornell <xy97@cornell.edu>
2018-10-05 13:59:14 -0400

    -a

--- lab02/combined_code/combined_code.ino
+++ lab02/combined_code/combined_code.ino
@@ -5,6 +5,13 @@
 #include <FFT.h> // include the library
 #include "IR_init.h"
 
+enum cases{
+    START,
+    AUDIO_FFT,
+    AUDIO_PROC,
+    IR_FFT,
+    IR_PROC
+};
 
 uint8_t state;
 
@@ -27,8 +34,52 @@ void setup() {
 void loop() {
   switch (state){
         
-        case 0:
+        case START:
+            Serial.println("starting");
             
+
+        case AUDIO_FFT:
+            Serial.println("AUDIO_FFT");
+            
+            for(int j=0;j<1;j++) { // reduces jitter
+                cli();  // UDRE interrupt slows this way down on arduino1.0
+                for (int i = 0 ; i < 512 ; i += 2) { // save 256 samples
+                    while(!(ADCSRA & 0x10)); // wait for adc to be ready
+                    ADCSRA = 0xf5; // restart adc
+                    byte m = ADCL; // fetch adc data
+                    byte j = ADCH;
+                    int k = (j << 8) | m; // form into an int
+                    k -= 0x0200; // form into a signed int
+                    k <<= 6; // form into a 16b signed int
+                    fft_input[i] = k; // put real data into even bins
+                    fft_input[i+1] = 0; // set odd bins to 0
+                }
+                fft_window(); // window the data for better frequency response
+                fft_reorder(); // reorder the data before doing the fft
+                fft_run(); // process the data in the fft
+                fft_mag_log(); // take the output of the fft
+                sei();   
+                
+                sum += fft_log_out[18];
+                sum/=N;
+            }
+            state = AUDIO_PROC;
+            break;
+        
+        case AUDIO_PROC:
+            Serial.println("AUDIO_PROC");
+            Serial.println(sum);
+            if(sum > 90){
+                turn_led(1);
+            }else{
+                turn_led(0);
+            }
+            sum = 0;
+            state = IR_FFT;
+            break;
+
+        case IR_FFT:
+            Serial.println("IR_FFT");
             for(int j=0;j<N;j++) { // reduces jitter
                 cli();  // UDRE interrupt slows this way down on arduino1.0
                 for (int i = 0 ; i < 512 ; i += 2) { // save 256 samples
@@ -50,11 +101,11 @@ void loop() {
                 
                 sum += fft_log_out[40];
             }
-            state ++;
-            break;
-
-        case 1:
             sum/=N;
+            state = IR_PROC;
+
+        case IR_PROC:
+            Serial.println("IR_PROC");
             Serial.println(sum);
             if(sum>55){
                 turn_led(1);
@@ -62,7 +113,7 @@ void loop() {
                 turn_led(0);
             }
             sum = 0;
-            state = 0;
+            state = START;
             break;
   }  
  
