f77647681302b6b260adb9dbd2dff5e1222e0725
taravns <42754343+taravns@users.noreply.github.com>
2018-10-22 12:32:08 -0400

    Update maze.h

--- lab03/basestation/maze.h
+++ lab03/basestation/maze.h
@@ -21,11 +21,11 @@ int * decoder(uint16_t input){
     bool left  = input & 0x1;
     bool front = (input >> 1) & 0x1;
     bool right  = (input >> 2) & 0x1;
-    //bool north = (input >> 3) & 0x1;
+    bool north = (input >> 3) & 0x1;
     bool robot = (input >> 8) & 0x1;
     int treasure = (input >> 3) & 0x7;
-    //int tshape = (input>>4) & 0x1;
-    //int tcolor =  (input>>5) & 0x1;
+    int tshape = (input>>4) & 0x1;
+    int tcolor =  (input>>5) & 0x1;
     int direction = (input>>6) & 0x3;
     int output[]={left, front, right, robot, treasure, direction};
     return output; 
