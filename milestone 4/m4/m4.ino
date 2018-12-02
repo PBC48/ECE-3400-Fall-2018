c87e12c4b241e3f07da70ef892b4f64944df5abe
xy97cornell <xy97@cornell.edu>
2018-12-02 14:34:07 -0500

    -a

--- milestone 4/m4/m4.ino
+++ milestone 4/m4/m4.ino
@@ -1,13 +1,7 @@
 /*
-milestone 4 code with FSMs where we integrated line sensor, 
-wall sensor, robot movement, microphone, ir, muxes, radio, 
-image processing all in one.
-
-Copied from milestone3-4.ino. The difference is the addition 
-of image processing file for treasure detection
-
+milestone 2 code with FSMs where we integrated line sensor, wall 
+sensor, robot movement, microphone, and ir all in one.
 */
-
 
 #define WAITTIME 800
 #define BUTTON 7 //Digital pin for button
@@ -22,7 +16,6 @@ of image processing file for treasure detection
 
 byte dir; // move_dir
 
-
 enum states : uint8_t
 {
     START,
@@ -34,7 +27,8 @@ enum states : uint8_t
     ROBOT_TURN_RIGHT,
     ROBOT_FORWARD,
     ROBOT_U_TURN,
-    TRANSMIT
+    TRANSMIT,
+    TREASURE_DECT
 };
 
 bool radio_msg_valid;
@@ -47,36 +41,29 @@ uint16_t LEFTWALL;
 uint16_t RIGHTWALL;
 
 
-void send_to_baseStation()
+void setup()
 {
-    robot_move(rstop);
-    servo_detach();
-    line_sensor_detach();
-    while(!radio_transmit(radio_msg));
-    line_sensor_init();
-    radio_msg_valid = false;
-}
-
-
-void setup() {
     Serial.begin(115200);
     line_sensor_init();
     radio_init(role_ping_out);
     STATE = START;
     u32wait = millis();
     mux_init();
-    pinMode(BUTTON,INPUT_PULLUP);
-    for (int i=0; i<9; i++){
-      for(int j=0; j<9; j++){
-        explored[i][j] = false;
-        visited_array[i][j] = 0;
-      }
+    pinMode(BUTTON, INPUT_PULLUP);
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            explored[i][j] = false;
+            visited_array[i][j] = 0;
+        }
     }
-    robot_pos = 0x08; //x=0,y=8
-    robot_dir = 2; //down
-    Serial.println("INITIAL STATE");
+    robot_pos = 0x00; //x=0,y=8
+    robot_dir = 1;    //down
+    Serial.println(F("INITIAL STATE"));
     // print_robot_state;
     radio_msg_valid = false;
+    treasure_setup();
 }
 
 void loop()
@@ -99,7 +86,7 @@ void loop()
         calculate_FFT(MIC);
         Serial.print(F("AUDIO SUM: "));
         Serial.println(sum);
-        if ((sum > 150) || (!digitalRead(BUTTON)) )
+        if ((sum > 150) || (!digitalRead(BUTTON)))
         {
             Serial.println(F("660Hz Tone Detected"));
             STATE = IR_DECT;
@@ -141,101 +128,119 @@ void loop()
         //Serial.println(F("IN ROBOT_SENSE"));
         //Serial.print(F("SENSOR_R READING: "));Serial.println(SENSOR_R_READING);
         //Serial.print(F("SENSOR_L READING: "));Serial.println(SENSOR_L_READING);
-            
-        if (AVERAGE_L < 200 && AVERAGE_R < 200 && VALID_L && VALID_R)
+
+        if (AVERAGE_L < 250 && AVERAGE_R < 250 && VALID_L && VALID_R)
         {
             Serial.print(F("SENSOR_R READING: "));
             Serial.println(AVERAGE_R);
             Serial.print(F("SENSOR_L READING: "));
             Serial.println(AVERAGE_L);
             Serial.println(F("In intersection"));
-            
+
             radio_msg = 0;
 
             // READ WALL SENSORS
-            set_mux_select(2);
-            FRONTWALL = analogRead(MUX_OUT);
-            Serial.print(F("FRONTWALL: "));Serial.println(FRONTWALL);
             set_mux_select(3);
             LEFTWALL = analogRead(MUX_OUT);
-            Serial.print(F("LEFTWALL: "));Serial.println(LEFTWALL);
+            ///////////////// START TREASURE DETECTION CODE ////////////////////
+            if (LEFTWALL < 100)
+            {
+                robot_move(forward);
+                delay(100);
+                int treasure = treasure_decode();
+                radio_msg |= (treasure & 0x3) << 3;
+            }
+            ///////////////// END TREASURE DETECTION CODE ////////////////////
+            robot_rest();
+            set_mux_select(2);
+            FRONTWALL = analogRead(MUX_OUT);
+            Serial.print(F("FRONTWALL: "));
+            Serial.println(FRONTWALL);
+
+            Serial.print(F("LEFTWALL: "));
+            Serial.println(LEFTWALL);
             set_mux_select(4);
             RIGHTWALL = analogRead(MUX_OUT);
-            Serial.print(F("RIGHTWALL: "));Serial.println(RIGHTWALL);
-           
-            explored[(robot_pos>>4) & 0x0f][(robot_pos) & 0x0f] = true;
-            visited_array[(robot_pos>>4) & 0x0f][(robot_pos) & 0x0f] = vcount;
+            Serial.print(F("RIGHTWALL: "));
+            Serial.println(RIGHTWALL);
+
+            explored[(robot_pos >> 4) & 0x0f][(robot_pos)&0x0f] = true;
+            visited_array[(robot_pos >> 4) & 0x0f][(robot_pos)&0x0f] = vcount;
             print_explored();
             print_robot_state();
             print_visited_array();
-            
+
             // DECIDE WHICH WAY TO GO
             // DFS: search order forward, right, left, back
-            if((FRONTWALL < 100) && (!is_explored(0))) 
+            if ((FRONTWALL < 100) && (!is_explored(0)))
             {
-              dir = 0;
-              Serial.println(F("Go Forward"));
-              vcount++;
-            } 
-            else if ((RIGHTWALL < 100) && (!is_explored(1))) 
+                dir = 0;
+                Serial.println(F("Go Forward"));
+                vcount++;
+            }
+            else if ((RIGHTWALL < 100) && (!is_explored(1)))
             {
-              dir = 1;
-              Serial.println(F("Turn Right"));
-              vcount++;
-            } 
-            else if ((LEFTWALL < 100) && (!is_explored(3))) 
+                dir = 1;
+                Serial.println(F("Turn Right"));
+                vcount++;
+            }
+            else if ((LEFTWALL < 100) && (!is_explored(3)))
             {
-              dir = 3;
-              Serial.println(("Turn Left"));
-              vcount++;
-              //visited.push(robot_pos); // add robot_pos to stack
-            } 
-            else 
+                dir = 3;
+                Serial.println(("Turn Left"));
+                vcount++;
+                //visited.push(robot_pos); // add robot_pos to stack
+            }
+            else
             {
-              next_pos = find_next_square();
-              dir = go_to(next_pos);
-              //dir = 2;
-              //Serial.println("Turn Around");
-              Serial.println(F("Backtrack"));
-              vcount = visited_array[(next_pos>>4) & 0x0f][(next_pos) & 0x0f];
+                next_pos = find_next_square();
+                dir = go_to(next_pos);
+                //dir = 2;
+                //Serial.println("Turn Around");
+                Serial.println(F("Backtrack"));
+                vcount = visited_array[(next_pos >> 4) & 0x0f][(next_pos)&0x0f];
             }
             // Check when and how vcount is set
             // Check assumptions made about vcount in functions that use it
             // Check when and how explored is set
             // Check when and how visited_array is set
             // Make sure the equation for move_dir in go_to is correct (subtraction)
-            
+
             // CHANGE FSM STATE
-            switch(dir){ // switch statement for dir to update fsm state
-              case 0: //forward
+            switch (dir)
+            {       // switch statement for dir to update fsm state
+            case 0: //forward
                 STATE = ROBOT_FORWARD;
                 break;
-              case 1: //right
+            case 1: //right
                 STATE = ROBOT_TURN_RIGHT;
                 break;
-              case 2: //back
+            case 2: //back
                 STATE = ROBOT_U_TURN;
                 break;
-              case 3: //left
+            case 3: //left
                 STATE = ROBOT_TURN_LEFT;
                 break;
             }
-            
-            // UPDATE ROBOT INFO VARIABLES
-            //explored[(robot_pos>>4) & 0x0f][(robot_pos) & 0x0f] = true; --- was here
-            update_robot_state(dir); // update robot_pos and robot_dir
 
             // TRANSMIT TO BASESTATION
             //radio_msg = millis();
-            radio_msg = 0xFFFF & (robot_pos << 8 |                                              //setting valid bit.
-                                  ((uint8_t)robot_dir << 6) | ((LEFTWALL > 115)) | (FRONTWALL > 115)<<1)| ((RIGHTWALL > 115)<<2);
+            radio_msg = 0xFFFF & (robot_pos << 8 | //setting valid bit.
+                                  ((uint8_t)robot_dir << 6) | ((LEFTWALL > 115)) | (FRONTWALL > 115) << 1) |
+                        ((RIGHTWALL > 115) << 2);
             /*completely re-work basestation communication?
               make sure dir in basestation is FRBL */
+
+            // UPDATE ROBOT INFO VARIABLES
+            //explored[(robot_pos>>4) & 0x0f][(robot_pos) & 0x0f] = true; --- was here
+            update_robot_state(dir); // update robot_pos and robot_dir
 
             // RESET VARIABLES
             u32wait = millis();
             VALID_L = false;
             VALID_R = false;
+            radio_msg_valid = true;
+            robot_wake();
         }
         else
         {
@@ -273,15 +278,24 @@ void loop()
 
     case ROBOT_TURN_LEFT:
         robot_move(left);
-        if (millis() - u32wait > 700)
+        //Serial.println(F("TURNING LEFT"));
+        if (millis() - u32wait > 750)
         {
             STATE = ROBOT_SENSE;
+            // robot_rest(); //stop all functions
+            // set_mux_select(3); //Check left wall
+            // int reading = analogRead(MUX_OUT);
+            // if(reading<100){
+            //     treasure_decode();
+            // }
+            // robot_wake();
         }
         break;
 
     case ROBOT_TURN_RIGHT:
         robot_move(right);
-        if (millis() - u32wait > 700)
+        //Serial.println(F("TURNING RIGHT"));
+        if (millis() - u32wait > 750)
         {
             STATE = ROBOT_SENSE;
         }
@@ -297,7 +311,7 @@ void loop()
 
     case ROBOT_U_TURN:
         robot_move(right);
-        if(millis()-u32wait > 1600)
+        if (millis() - u32wait > 1650)
         {
             STATE = ROBOT_SENSE;
         }
@@ -305,7 +319,7 @@ void loop()
 
     case TRANSMIT:
         send_to_baseStation();
-        
+
         STATE = ROBOT_SENSE;
         break;
 
@@ -314,4 +328,27 @@ void loop()
         Serial.println(F("state broken"));
         break;
     }
+}
+
+
+void send_to_baseStation()
+{
+    robot_move(rstop);
+    servo_detach();
+    line_sensor_detach();
+    while (!radio_transmit(radio_msg))
+        ;
+    line_sensor_init();
+    radio_msg_valid = false;
+}
+
+void robot_rest()
+{
+    robot_move(rstop);
+    servo_detach();
+    line_sensor_detach();
+}
+void robot_wake()
+{
+    line_sensor_init();
 }
--- milestone 4/m4/treasure.h
+++ milestone 4/m4/treasure.h
@@ -14,30 +14,6 @@
  
 
 
-
-///////// Main Program //////////////
-void treasure_setup() {
-  Wire.begin();
-  
-  Serial.println(OV7670_write_register(0x12,0x80)); //COM7 reset all regs
-//  Serial.println(OV7670_write_register(0x12,0x0E)); //COM7 enable color bar test and QCIF
-//  Serial.println(OV7670_write_register(0x42,0x08)); //COM17 enables color bar DSP
-  
-  Serial.println(OV7670_write_register(0x12,0x0c)); //COM7 QCIF 176x144 resolution  
-  Serial.println(OV7670_write_register(0x0c,0x08)); //COM3 Enable Scaling
-  Serial.println(OV7670_write_register(0x14,0x61)); //COM9 reduce noise
-  Serial.println(OV7670_write_register(0x40,0xD0)); //COM15 select output range and RGB565
-  Serial.println(OV7670_write_register(0x11,0xC0)); //CLKRC two clk both same speed, use external clk
-  Serial.println(OV7670_write_register(0x1E,0x30)); //MVFP flip/mirror | was at 0x30
-  Serial.println("Wrote registers");
-  //set_color_matrix();
-  //read_key_registers();
-  Serial.println("finished");
-  init_com();
-}
-
-///////// Function Definition //////////////
-
 void init_com(){
   pinMode(T1,INPUT);
   pinMode(T2,INPUT);
@@ -45,99 +21,114 @@ void init_com(){
   pinMode(C2,INPUT);
 }
 
-uint8_t decoder(){
-  uint8_t treasure1 = digitalRead(T1);
-  uint8_t treasure2 = digitalRead(T2);
-  uint16_t color1   = digitalRead(C1);
-  uint16_t color2   = digitalRead(C2);
-  Serial.print("T1: ");Serial.print(treasure1);
-  Serial.print(" T2: ");Serial.print(treasure2);
-  Serial.print(" C1: ");Serial.print(color1);
-  Serial.print(" C2: ");Serial.print(color2);
+
+///////// Function Definition //////////////
+
+
+uint8_t treasure_decode(){
+  uint8_t treasure1,treasure2,color1,color2;
+  uint8_t treasure;
+  uint8_t color  ;
+  int redCount = 0, blueCount = 0,
+  diaCount = 0, triCount = 0, sqCount = 0, nonCount = 0, nonTreasure = 0;
+  for (int i = 0; i<100; i++){
+    treasure1 = digitalRead(T1);
+    treasure2 = digitalRead(T2);
+    color1     = digitalRead(C1);
+    color2     = digitalRead(C2);
+    treasure  = (treasure2 << 1)|treasure1;
+    color   =   (color2<<1)|(color1);
+    if(color==BLUE) blueCount += 1;
+    else if(color==RED)   redCount += 1;
+    else nonCount += 1;
+    if (treasure == SQUARE) sqCount +=1;
+    else if( treasure == TRIANGLE) triCount += 1;
+    else if( treasure == DIAMOND)  diaCount += 1;
+    else nonTreasure += 1;
+    delay(10);
+  }
+//   Serial.print("T1: "); Serial.print(treasure1);
+//   Serial.print(" T2: ");Serial.print(treasure2);
+//   Serial.print(" C1: ");Serial.print(color1);
+//   Serial.print(" C2: ");Serial.print(color2);
   
-  uint8_t treasure  = (treasure2 << 1)|treasure1;
-  uint8_t color   =   (color2<<1)|(color1);
-  Serial.println("");
-  Serial.print("---COLOR: ");
-  if(color==BLUE) Serial.print("BLUE");
-  else if(color==RED)    Serial.print("RED");
-  else             Serial.print("NONE");
-  Serial.print( " Treasure: ");
-  if( treasure == SQUARE)
-      Serial.print("SQUARE");
-  else if( treasure == TRIANGLE)
-      Serial.print("TRIANGLE");
-  else if( treasure == DIAMOND)
-      Serial.print("DIAMOND");
-  else
-      Serial.print("NONE");
-  Serial.println("");
+  Serial.println(F(""));
+  Serial.print(F("---COLOR: "));
+  if( (blueCount>nonCount || redCount > nonCount) && blueCount>redCount) {
+    Serial.print(F("BLUE"));
+    color = BLUE;
+  }
+  else if((blueCount>nonCount || redCount > nonCount) && redCount>blueCount)  {  
+    Serial.print(F("RED"));
+    color = RED;
+  }
+  else{
+       Serial.print(F("NONE"));
+       color = NONE;
+  }             
+ 
+  Serial.print( F(" Treasure: "));
+  if( ((sqCount>nonTreasure)||(triCount>nonTreasure)||(diaCount>nonTreasure)) 
+  && (sqCount > triCount) && (sqCount > diaCount)){
+       Serial.print(F("SQUARE"));
+       treasure = SQUARE;
+  }
+     
+  else if( ((sqCount>nonTreasure)||(triCount>nonTreasure)||(diaCount>nonTreasure)) 
+  && (sqCount < triCount) && (triCount > diaCount)){
+      Serial.print(F("TRIANGLE"));
+      treasure = TRIANGLE;
+  }   
+  else if( ((sqCount>nonTreasure)||(triCount>nonTreasure)||(diaCount>nonTreasure)) 
+  && (diaCount > triCount) && (triCount < diaCount)){
+      Serial.print(F("DIAMOND"));
+      treasure = DIAMOND;
+  }
+  else{
+      Serial.print(F("NONE"));
+      treasure= NONE;
+  }
+
   // 0: NONE, 
   // 1: tr, 2: sr, 3: dr
   // 4: tb, 5: sb, 6: db 
+  if (treasure == NONE && color == NONE){
+      return 0;
+  }else if(color==RED){
+      if(treasure==TRIANGLE){
+          return 1;
+      }
+      else if(treasure==SQUARE){
+          return 2;
+      }
+      else {
+          return 3;
+      }
+  }else if(color == BLUE){
+      if(treasure==TRIANGLE){
+          return 4;
+      }
+      else if(treasure==SQUARE){
+          return 5;
+      }
+      else {
+          return 6;
+      }
+  }else{
+      return 0;
+  }
+
+  Serial.println("");
+
+  Serial.print(F("red="));Serial.print(redCount);Serial.print(F(" blue="));Serial.print(blueCount);
+  Serial.print(F(" nonCount="));Serial.print(nonCount);
+  Serial.print(F(" sq="));Serial.print(sqCount);Serial.print(F(" tri="));Serial.print(triCount);
+  Serial.print(F(" dia="));Serial.print(diaCount);Serial.print(F(" nonTreasure="));Serial.print(nonTreasure);
+  Serial.println("");
     
 }
 
-
-
-void read_key_registers(){
-  Serial.println("read key regs");
-
-    ReadOV7670(0x12);
-    ReadOV7670(0x0c);
-    ReadOV7670(0x14);
-    ReadOV7670(0x40);
-    ReadOV7670(0x42);
-    ReadOV7670(0x11);
-    ReadOV7670(0x1e);
-}
-
-String OV7670_write_register(int reg_address, byte data){
-  return OV7670_write(reg_address, &data, 1);
- }
-
-void set_color_matrix(){
-    OV7670_write_register(0x4f, 0x80);
-    OV7670_write_register(0x50, 0x80);
-    OV7670_write_register(0x51, 0x00);
-    OV7670_write_register(0x52, 0x22);
-    OV7670_write_register(0x53, 0x5e);
-    OV7670_write_register(0x54, 0x80);
-    OV7670_write_register(0x56, 0x40);
-    OV7670_write_register(0x58, 0x9e);
-    OV7670_write_register(0x59, 0x88);
-    OV7670_write_register(0x5a, 0x88);
-    OV7670_write_register(0x5b, 0x44);
-    OV7670_write_register(0x5c, 0x67);
-    OV7670_write_register(0x5d, 0x49);
-    OV7670_write_register(0x5e, 0x0e);
-    OV7670_write_register(0x69, 0x00);
-    OV7670_write_register(0x6a, 0x40);
-    OV7670_write_register(0x6b, 0x0a);
-    OV7670_write_register(0x6c, 0x0a);
-    OV7670_write_register(0x6d, 0x55);
-    OV7670_write_register(0x6e, 0x11);
-    OV7670_write_register(0x6f, 0x9f);
-    OV7670_write_register(0xb0, 0x84);
-}
-  void ReadOV7670(byte regID){
-  // Reading from a register is done in two steps
-  // Step 1: Write register address to the slave
-  // from which data is to be read.
-  Wire.beginTransmission(0x21); // 7-bit Slave address
-  Wire.write(regID);  // reading from register
-  Wire.endTransmission();
  
-  // Step 2: Read 1 byte from Slave
-  Wire.requestFrom(0x21, 1);
-  Serial.print("Read request Status:");
-  Serial.println(Wire.available());
-  Serial.print(regID,HEX);
-  Serial.print(":");
-  Serial.println(Wire.read(),HEX);
-  }
-
-  
 String OV7670_write(int start, const byte *pData, int size){
     int n,error;
     Wire.beginTransmission(OV7670_I2C_ADDRESS);
@@ -156,6 +147,29 @@ String OV7670_write(int start, const byte *pData, int size){
     return "no errors :)";
  }
 
+String OV7670_write_register(int reg_address, byte data){
+  return OV7670_write(reg_address, &data, 1);
+ }
+
+void ReadOV7670(byte regID){
+  // Reading from a register is done in two steps
+  // Step 1: Write register address to the slave
+  // from which data is to be read.
+  Wire.beginTransmission(0x21); // 7-bit Slave address
+  Wire.write(regID);  // reading from register
+  Wire.endTransmission();
+ 
+  // Step 2: Read 1 byte from Slave
+  Wire.requestFrom(0x21, 1);
+  Serial.print("Read request Status:");
+  Serial.println(Wire.available());
+  Serial.print(regID,HEX);
+  Serial.print(":");
+  Serial.println(Wire.read(),HEX);
+  }
+
+ 
+
 byte read_register_value(int register_address){
   byte data = 0;
   Wire.beginTransmission(OV7670_I2C_ADDRESS);
@@ -167,4 +181,36 @@ byte read_register_value(int register_address){
   Serial.println("after while");
   data = Wire.read();
   return data;
+}
+
+///////// Main Program //////////////
+void treasure_setup() {
+  Wire.begin();
+  Serial.println(F("treasure_setup"));
+  Serial.println(OV7670_write_register(0x12,0x80)); //COM7 reset all regs
+//  Serial.println(OV7670_write_register(0x12,0x0E)); //COM7 enable color bar test and QCIF
+//  Serial.println(OV7670_write_register(0x42,0x08)); //COM17 enables color bar DSP
+  
+  Serial.println(OV7670_write_register(0x12,0x0c)); //COM7 QCIF 176x144 resolution  
+  Serial.println(OV7670_write_register(0x0c,0x08)); //COM3 Enable Scaling
+  Serial.println(OV7670_write_register(0x14,0x61)); //COM9 reduce noise
+  Serial.println(OV7670_write_register(0x40,0xD0)); //COM15 select output range and RGB565
+  Serial.println(OV7670_write_register(0x11,0xC0)); //CLKRC two clk both same speed, use external clk
+  Serial.println(OV7670_write_register(0x1E,0x30)); //MVFP flip/mirror | was at 0x30
+  Serial.println("Wrote registers");
+  //set_color_matrix();
+  //read_key_registers();
+  init_com();
+}
+
+void read_key_registers(){
+  Serial.println("read key regs");
+
+    ReadOV7670(0x12);
+    ReadOV7670(0x0c);
+    ReadOV7670(0x14);
+    ReadOV7670(0x40);
+    ReadOV7670(0x42);
+    ReadOV7670(0x11);
+    ReadOV7670(0x1e);
 }
